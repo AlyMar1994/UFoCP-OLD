e263a9c188c8984dd5a4d52fed57532e0639d186
Connor Hess (AlyMar1994) <ConnorSyphentoticsEmail@gmail.com>
2020-04-26 18:43:26 -0400

    Updated FireDeathStar script.

--- Data/Scripts/AI/SpaceMode/FireDeathStar.lua
+++ Data/Scripts/AI/SpaceMode/FireDeathStar.lua
@@ -4,7 +4,7 @@
 -- ORIGINAL AUTHOR (Petroglyph): Steve Copeland
 -- NEW AUTHOR: Connor "AlyMar1994" Hess
 --
--- LAST REVISION DATE: 3/14/2020, 11:12 AM
+-- LAST REVISION DATE: 4/26/2020, 5:56 PM
 -- ======================================================================
 require("pgevents")
 
@@ -24,23 +24,24 @@ function Definitions()
 	DebugMessage("%s -- Done Definitions", tostring(Script))
 end
 
+-- AM1994 (4/26/2020): Thanks, Nikomer.
 function MainForce_Thread()
 	BlockOnCommand(MainForce.Produce_Force())
 
-	-- ds = Find_First_Object("Death_Star")
-	ds = Find_First_Object("Death_Star_II")
-	while not TestValid(ds) do
-		-- Tactical Superweapon(s) aren't present, hang this plan indefinately!
-		Sleep(100)
+	ds = Find_First_Object("Death_Star")
+	if not TestValid(ds) then
+		ds = Find_First_Object("Death_Star_II")
 	end
 
-	repeat
-		Sleep(1)
-	until ds.Is_Tactical_Superweapon_Ready() and (Evaluate_In_Galactic_Context("Want_To_Fire_DS", PlayerObject) ~= 0)
+	if TestValid(ds) then
+		-- Tactical Superweapon(s) are present, continue plan!
+		repeat
+			Sleep(1)
+		until ds.Is_Tactical_Superweapon_Ready() and (Evaluate_In_Galactic_Context("Want_To_Fire_DS", PlayerObject) ~= 0)
 
-	ds.Fire_Tactical_Superweapon()
+		ds.Fire_Tactical_Superweapon()
+	end
 
 	Sleep(30)
-
 	ScriptExit()
 end
