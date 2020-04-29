2003be701b972462cddfbb82b13f10629559b785
Connor Hess (AlyMar1994) <ConnorSyphentoticsEmail@gmail.com>
2020-04-29 15:04:50 -0400

    Update FireDeathStar.lua
    - Adding explanation comment.

--- Data/Scripts/AI/SpaceMode/FireDeathStar.lua
+++ Data/Scripts/AI/SpaceMode/FireDeathStar.lua
@@ -24,7 +24,11 @@ function Definitions()
 	DebugMessage("%s -- Done Definitions", tostring(Script))
 end
 
--- AM1994 (4/26/2020): Thanks, Nikomer.
+-- AM1994 (4/29/2020): function MainForce_Thread() was overhauled as a
+-- future-proof fear that users won't be able to have the AI fire the DS1,
+-- due to the code only focusing on finding the DS2. If someone uses
+-- UFoCP as a base, and changes the buildable DS2 to 1, the AI may now fire it.
+-- Thanks, Nikomer.
 function MainForce_Thread()
 	BlockOnCommand(MainForce.Produce_Force())
 
@@ -34,7 +38,7 @@ function MainForce_Thread()
 	end
 
 	if TestValid(ds) then
-		-- Tactical Superweapon is present, continue plan!
+		-- Tactical superweapon is present, continue plan!
 		repeat
 			Sleep(1)
 		until ds.Is_Tactical_Superweapon_Ready() and (Evaluate_In_Galactic_Context("Want_To_Fire_DS", PlayerObject) ~= 0)
